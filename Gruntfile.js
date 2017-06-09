ac4b2cb788d80f393f3fbc1d634c3adcad089c52
fgcintra <fausto@fgcintra.pro.br>
2017-06-08 22:08:43 -0300

    Terminada a configuração das tarefas de deploy com Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,25 @@ module.exports = function(grunt) {
          dist: {
             src: 'dist'
          }
+      },
+
+      usemin: {
+         html: 'dist/app/views/**/*.ejs'
+      },
+
+      useminPrepare: {
+         html: 'dist/app/views/**/*.ejs',
+         options: {
+            root: 'dist/public',
+            dest: 'dist/public'
+         }
+      },
+
+      ngAnnotate: {
+         scripts: {
+            expand: true,
+            src: ['dist/public/js/**/*.js']
+         }
       }
 
    });
@@ -28,9 +47,17 @@ module.exports = function(grunt) {
 
    // 'dist' será a tarefa chamada por padrão, se nenhuma outra
    // for passada ao grunt
-   grunt.registerTask('default', ['dist']);
+   grunt.registerTask('default', ['dist', 'minifica']);
+
+   grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate',
+      'concat', 'uglify', 'cssmin', 'usemin']);
    
    grunt.loadNpmTasks('grunt-contrib-copy');
    grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
+   grunt.loadNpmTasks('grunt-usemin');
+   grunt.loadNpmTasks('grunt-ng-annotate');
 
 }
